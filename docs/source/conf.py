ddcec6d3e2b1928a18f0f3cbafcc602c65f95f63
xshadowbringer <123817480+xshadowbringer@users.noreply.github.com>
2023-03-30 20:25:53 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Shadow-Robot'
+copyright = '2023, xshadowbringer'
+author = 'xshadowbringer'
 
 release = '0.1'
 version = '0.1.0'
